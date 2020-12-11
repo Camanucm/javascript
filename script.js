88ff00a35f696c093d9e7c1be14ea5f0b7097e34
Andy <legion_rf@list.ru>
2020-12-11 14:53:31 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict';
+let Tc = +prompt('Введите температуру в цельсиях');
+let Tf = (9/5)*Tc + 32;
+alert('Температура в фарингетах ' + Tf);
+let name = 'Василий';
+let admin = name;
+alert(admin);
